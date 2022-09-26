3badbf5ad807b31b5cbff5f14fab044dcf3e3fe3
Robles Lautaro <lautaro0121@gmail.com>
2022-09-26 00:16:24 -0300

    navbar

--- src/component/About/About.jsx
+++ src/component/About/About.jsx
@@ -3,7 +3,26 @@ export default function About () {
 
     return (
         <section className="about-conteiner" id='About'>
-            <h1> ABOUT ME </h1>
+            <div className='about-card'> 
+                <div>
+                    <h1> ABOUT ME </h1>
+                    <p>A Short Story about me.................................................</p>
+                </div>
+                <aside>
+                    <h2> Technologies </h2>
+                    <div>
+                        <ul>
+                            <li>TP</li>
+                            <li>JS</li>
+                            <li>HTML</li>
+                            <li>CSS</li>
+                        </ul>
+                    </div>
+                    <div>
+                        <p>afsdfasdfds</p>
+                    </div>
+                </aside>
+            </div>
         </section>
     )
 }
--- src/component/NavBar/NavBar.jsx
+++ src/component/NavBar/NavBar.jsx
@@ -5,7 +5,7 @@ export default function NavBar () {
     const [color, setColor] = useState(false)
 
     const handleColor = () => {
-        if(window.scrollY>=500){
+        if(window.scrollY>=100){
             setColor(true)
         } else {
             setColor(false)
@@ -17,10 +17,10 @@ export default function NavBar () {
         <header className={color ? "nav-conteiner-below" : "navbar-conteiner"}>
             <nav>
                 <Scrollspy className={color ? 'navbar-buttons-below' : 'navbar-buttons'} items={ ['Home', 'About', 'Projects', 'Contact'] } currentClassName={color ? "is-current-below" : "is-current"}>
-                    <li><a href='#page-top'>Home</a></li>
-                    <li><a href='#About'>About</a></li>
-                    <li><a href='#Projects'>Projects</a></li>
-                    <li><a href='#Contact'>Contact</a></li>
+                    <li><a href='#Home'>HOME</a></li>
+                    <li><a href='#About'>ABOUT</a></li>
+                    <li><a href='#Projects'>PROJECTS</a></li>
+                    <li><a href='#Contact'>CONTACT</a></li>
                 </Scrollspy>
             </nav>
         </header>
